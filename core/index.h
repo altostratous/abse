a6d30a4b9be20e80d159a42ca8983477166d3489
arasekh <a.rasekh75@gmail.com>
2015-12-18 06:47:42 -0800

    کمی مرحله اول فاز یک را جلو بردیم.

--- core/index.h
+++ core/index.h
@@ -1,1 +1,27 @@
-// to do Indexing
+/*
+	Purpose: to index the content
+*/
+
+#ifndef ABSE_INDEX
+#define ABSE_INDEX 1
+
+#include<vector>
+#include<ds.h>
+
+using namespace ds;
+
+namespace index
+{
+	class wcounter
+	{
+		private:
+			vector<occurrance> occurrances;
+		public:
+			int getCount()
+			{
+			
+			}
+	}
+}
+
+#endif
