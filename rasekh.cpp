e2cf725ec349ee2b53251a93894277f9bf1bdec8
arasekh <a.rasekh75@gmail.com>
2015-12-20 09:13:31 -0800

    replace function is finished.

--- rasekh.cpp
+++ rasekh.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string replace(string input, string src, string dst)
+{
+	if(input == src)
+		return dst;
+	else
+		return input;
+}
+
 
