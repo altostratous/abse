886f276ffa7717bca1c59c221729fda091f4a4a6
arasekh <a.rasekh75@gmail.com>
2015-12-24 13:10:35 -0800

    sort functions added.

---
+++ sort.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#include "ds.h"
+#include<vector>
+using namespace std;
+using namespace ds;
+
+
+bool checkCount (wanalysis* g, wanalysis* a) { return (g->getCount() < a->getcount()); }
+bool checkWord (wanalysis* g, wanalysis* a) { return (g-> getword()< a->getword)(); }
+void sort(vector<wanalysis>& was)
+{
+	sort(was.begin(),was.end(), checkCount)
+	sort(was.begin(),was.end(), checkWord)
+}
