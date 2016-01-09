af6474e95557f498f74df8e64a0d20e3923105b4
altostratous <altostratous@gmail.com>
2016-01-09 09:28:03 +0330

    Added simple trienode from assignment "توافق هسته ای"

--- core/ds.h
+++ core/ds.h
@@ -28,9 +28,83 @@ namespace ds
 		int paragraph_id;
 		int line_id;
 	};
-	/* TODO (rasekh#1#): Add datastructure for dictionary and spell 
+	/* DONE (asgari#1#): Add datastructure for dictionary and spell 
 	                     check and suggest */
+	                     
 	
+	class trienode
+	{
+	    private:
+	        trienode* parent;
+	        trienode* children[50];
+	        char keypart;
+	        int count;
+	        bool is_end;
+	        void addchild(trienode* child)
+	        {
+	            children[count] = child;
+	            child->parent = this;
+	            count++;    
+	        }
+	    public:
+	        trienode(trienode* parent, char keypart)
+	        {
+	            is_end = false;
+	            this->parent = parent;
+	            count = 0;
+	            this->keypart = keypart;
+	        }
+	        void addtrienode(string key)
+	        {
+	            if(key.length() == 0)
+	            {
+	                is_end = true;
+	                return;
+	            }
+	            int i;
+	            bool test = false;
+	            for(i = 0; i < count; i++)
+	            {
+	                if(children[i]->keypart == key[0])
+	                {
+	                    test = true;
+	                    break;
+	                }
+	            }
+	            if(test)
+	            {
+	                children[i]->addtrienode(key.substr(1));
+	            }
+	            else
+	            {
+	                trienode* ch = new trienode(this, key[0]);
+	                addchild(ch);
+	                addtrienode(key);
+	            }
+	        }
+	        
+	        bool haskey(string key)
+	        {
+	            if(key.length() == 0)
+	            {
+	                return is_end;
+	            }
+	            bool test = false;
+	            int i;
+	            for(i = 0; i < count; i++)
+	            {
+	                if(children[i]->keypart == key[0])
+	                {
+	                    test = true;
+	                    break;
+	                }
+	            }
+	            if(test)
+	                return children[i]->haskey(key.substr(1));
+	            else
+	                return false;
+	        }
+	};
 }
 
 #endif
